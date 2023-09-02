f792b2e2a000045f754af6b003bb4b22fc63eb19
Damith Anushka <damithanushkam016@gmail.com>
2023-09-02 11:28:11 +0530

    Added an one form

--- javaFx-desktop-app/src/main/java/controller/MainFormController.java
+++ javaFx-desktop-app/src/main/java/controller/MainFormController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    @FXML
+    private TextField txtMsg;
+    @FXML
+    void btnClickOnAction(ActionEvent event) {
+        String text = txtMsg.getText();
+        System.out.println(text);
+    }
+
 }
